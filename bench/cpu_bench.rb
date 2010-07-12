403fbc2e9806fd110afa91232c89c17e21c03407
Jeremy Evans <code@jeremyevans.net>
2010-07-12 14:24:32 -0700

    Fix bench task to work with stdlib date code that requires Date constant

--- bench/cpu_bench.rb
+++ bench/cpu_bench.rb
@@ -1,16 +1,21 @@
 require 'date'
 require 'benchmark'
-D = Date
+SD = Date
 $:.unshift(File.join(File.dirname(File.dirname(File.expand_path(__FILE__))), 'ext'))
 load 'lib/date.rb'
+HRD = Date
 
 def compare(label, &block)
+  Object.send(:remove_const, :Date)
+  Object.send(:const_set, :Date, SD)
   t = Time.now
-  yield D
+  yield SD
   stdlib = Time.now - t
 
+  Object.send(:remove_const, :Date)
+  Object.send(:const_set, :Date, HRD)
   t = Time.now
-  yield Date
+  yield HRD
   home_run = Time.now - t
   
   puts sprintf('%s,%0.3f,%0.3f,%0.3f', label, stdlib, home_run, stdlib/home_run)
@@ -26,16 +31,16 @@ compare("#inspect"){|dc| d = dc.civil(2010, 1, 1); n.times{d.inspect}}
 compare("#to_s"){|dc| d = dc.civil(2010, 1, 1); n.times{d.to_s}}
 compare("#asctime"){|dc| d = dc.civil(2010, 1, 1); n.times{d.asctime}}
 compare("#jd"){|dc| d = dc.jd(2010); n.times{d.jd}}
-compare("#eql? Date"){|dc| d = dc.civil(2010, 1, 1); d2 = dc.civil(2010, 1, 1); n.times{d.eql? d2}}
+compare("#eql?"){|dc| d = dc.civil(2010, 1, 1); d2 = dc.civil(2010, 1, 1); n.times{d.eql? d2}}
 compare("#<=> Date"){|dc| d = dc.civil(2010, 1, 1); d2 = dc.civil(2010, 1, 2); n.times{d <=> d2}}
 compare("#<=> Integer"){|dc| d = dc.civil(2010, 1, 1); d2 = 2010; n.times{d <=> d2}}
 compare("#=== Date"){|dc| d = dc.civil(2010, 1, 1); d2 = dc.civil(2010, 1, 2); n.times{d === d2}}
 compare("#=== Integer"){|dc| d = dc.civil(2010, 1, 1); d2 = 2010; n.times{d === d2}}
-compare("#+ 100"){|dc| d = dc.civil(2010, 1, 1); n.times{d + 100}}
-compare("#- 100"){|dc| d = dc.civil(2010, 1, 1); n.times{d - 100}}
-#compare("#- dc"){|dc| d = dc.jd(2010); d2 = dc.jd(2011); n.times{d - d2}}
-compare("#>> 13"){|dc| d = dc.civil(2010, 1, 31); n.times{d >> 13}}
-compare("#<< 17"){|dc| d = dc.civil(2011, 7, 31); n.times{d << 17}}
+compare("#+"){|dc| d = dc.civil(2010, 1, 1); n.times{d + 100}}
+compare("#- Integer"){|dc| d = dc.civil(2010, 1, 1); n.times{d - 100}}
+compare("#- Date"){|dc| d = dc.jd(2010); d2 = dc.jd(2011); n.times{d - d2}}
+compare("#>>"){|dc| d = dc.civil(2010, 1, 31); n.times{d >> 13}}
+compare("#<<"){|dc| d = dc.civil(2011, 7, 31); n.times{d << 17}}
 compare("#next"){|dc| d = dc.civil(2010, 1, 1); n.times{d.next}}
 compare("#yday"){|dc| d = dc.civil(2010, 1, 1); n.times{d.yday}}
 compare("#wday"){|dc| d = dc.civil(2010, 1, 1); n.times{d.wday}}
@@ -47,6 +52,7 @@ compare("#ld"){|dc| d = dc.civil(2010, 1, 1); n.times{d.ld}}
 compare("#cwyear"){|dc| d = dc.civil(2010, 1, 1); n.times{d.cwyear}}
 compare("#cweek"){|dc| d = dc.civil(2010, 1, 1); n.times{d.cweek}}
 compare("#cwday"){|dc| d = dc.civil(2010, 1, 1); n.times{d.cwday}}
+compare("#step"){|dc| d = dc.civil(2010, 1, 1); d2 = dc.civil(2010, 1, 10); n.times{d.step(d2){}}}
 
 compare(".jd.inspect"){|dc|n.times{dc.jd(2010).inspect}}
 compare(".jd.(year|month|day)"){|dc| n.times{d = dc.jd(2010); d.year; d.month; d.day}}
