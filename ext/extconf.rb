2c8e73aa5cfa1cda5fe61768255bbd6625e18906
Jeremy Evans <code@jeremyevans.net>
2010-08-03 17:25:48 -0700

    Add a couple CFLAGS to remove pointless warnings

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,5 +1,5 @@
 require 'mkmf'
-$CFLAGS << " -Wall#{' -DRUBY19' if  RUBY_VERSION >= '1.9.0'}"
+$CFLAGS << " -O2 -Wno-extra -Wall#{' -DRUBY19' if  RUBY_VERSION >= '1.9.0'}"
 $CFLAGS << ' -g -ggdb -rdynamic -O0 -DDEBUG' if ENV['DEBUG']
 $objs = ['date.o']
 create_makefile("date")
