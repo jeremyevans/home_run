d79e235bfe119bcba5f157ad01c17d7107ac00ad
Jeremy Evans <code@jeremyevans.net>
2010-08-13 14:26:49 -0700

    Remove install/uninstall options from the Makefile

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,17 +4,4 @@ $CFLAGS << ' -Wno-extra' if $CFLAGS =~ /-Wextra/
 $CFLAGS << ' -g -ggdb -rdynamic -O0 -DDEBUG' if ENV['DEBUG']
 $objs = ['date.o']
 create_makefile("date")
-File.open('Makefile', 'ab') do |f|
-  f.puts <<END
-
-install-home-run: install-so
-	$(INSTALL_PROG) date.rb $(RUBYLIBDIR)
-	$(INSTALL_PROG) -d $(RUBYLIBDIR)/date
-	$(INSTALL_PROG) date/format.rb $(RUBYLIBDIR)/date
-
-uninstall-home-run:
-	rm -f $(RUBYARCHDIR)/date.so $(RUBYLIBDIR)/date.rb \
-		$(RUBYLIBDIR)/date/format.rb
-	rmdir $(RUBYLIBDIR)/date
-END
 end
