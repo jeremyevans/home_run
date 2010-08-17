839344da33caf1a498df05bb2832b03f75214306
Jeremy Evans <code@jeremyevans.net>
2010-08-16 18:04:19 -0700

    Add Rdoc for ruby 1.9 Date#*day? instance methods

--- ext/date.c
+++ ext/date.c
@@ -3440,32 +3440,74 @@ static VALUE rhrd_time_to_time(VALUE self) {
   return rb_funcall(self, rhrd_id_getlocal, 0);
 }
 
-/* 1.9 day? methods */
+/* Ruby 1.9 *day? instance methods */
 
+/* call-seq:
+ *   [ruby 1-9 only] <br />
+ *   sunday? -> true or false
+ *
+ * Returns +true+ if the receiver is a Sunday, +false+ otherwise.
+ */
 static VALUE rhrd_sunday_q(VALUE self) {
   return rhrd__day_q(self, 0);
 }
 
+/* call-seq:
+ *   [ruby 1-9 only] <br />
+ *   monday? -> true or false
+ *
+ * Returns +true+ if the receiver is a Monday, +false+ otherwise.
+ */
 static VALUE rhrd_monday_q(VALUE self) {
   return rhrd__day_q(self, 1);
 }
 
+/* call-seq:
+ *   [ruby 1-9 only] <br />
+ *   tuesday? -> true or false
+ *
+ * Returns +true+ if the receiver is a Tuesday, +false+ otherwise.
+ */
 static VALUE rhrd_tuesday_q(VALUE self) {
   return rhrd__day_q(self, 2);
 }
 
+/* call-seq:
+ *   [ruby 1-9 only] <br />
+ *   wednesday? -> true or false
+ *
+ * Returns +true+ if the receiver is a Wednesday, +false+ otherwise.
+ */
 static VALUE rhrd_wednesday_q(VALUE self) {
   return rhrd__day_q(self, 3);
 }
 
+/* call-seq:
+ *   [ruby 1-9 only] <br />
+ *   thursday? -> true or false
+ *
+ * Returns +true+ if the receiver is a Thursday, +false+ otherwise.
+ */
 static VALUE rhrd_thursday_q(VALUE self) {
   return rhrd__day_q(self, 4);
 }
 
+/* call-seq:
+ *   [ruby 1-9 only] <br />
+ *   friday? -> true or false
+ *
+ * Returns +true+ if the receiver is a Friday, +false+ otherwise.
+ */
 static VALUE rhrd_friday_q(VALUE self) {
   return rhrd__day_q(self, 5);
 }
 
+/* call-seq:
+ *   [ruby 1-9 only] <br />
+ *   saturday? -> true or false
+ *
+ * Returns +true+ if the receiver is a Saturday, +false+ otherwise.
+ */
 static VALUE rhrd_saturday_q(VALUE self) {
   return rhrd__day_q(self, 6);
 }
