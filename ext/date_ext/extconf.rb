6d39856f977ce2a4bbcfc03dd1d8c84dd1bfa8ac
Jeremy Evans <code@jeremyevans.net>
2010-09-01 10:35:23 -0700

    Include CFLAGS from environment when compiling
    Also, check the STRICT environment variable and add a bunch
    of warnings if it is present.

--- ext/date_ext/extconf.rb
+++ ext/date_ext/extconf.rb
@@ -3,4 +3,6 @@ $CFLAGS << " -DRUBY19" if  RUBY_VERSION >= '1.9.0'
 $CFLAGS << " -DRUBY186" if  RUBY_VERSION < '1.8.7'
 $CFLAGS << " -Wall " unless RUBY_PLATFORM =~ /solaris/
 $CFLAGS << ' -g -ggdb -rdynamic -O0 -DDEBUG' if ENV['DEBUG']
+$CFLAGS << " -Wconversion -Wsign-compare -Wno-unused-parameter -Wwrite-strings -Wpointer-arith -fno-common -pedantic -Wno-long-long" if ENV['STRICT']
+$CFLAGS << (ENV['CFLAGS'] || '')
 create_makefile("date_ext")
